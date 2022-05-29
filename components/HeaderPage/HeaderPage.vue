695c93f288eda576fc1abb61ed5f942066776a52
Amphorn <87352134+Amphorn@users.noreply.github.com>
2022-05-29 15:01:51 +0700

    add

---
+++ components/HeaderPage/HeaderPage.vue
@@ -0,0 +1,50 @@
+<template>
+<div class="container">
+   <div class="nav">
+       <ul class="nav-list nav-list-mobile">
+           <li class="nav-item">
+               <div class="mobile-menu">
+                   <span class="line line-top"></span>
+                   <span class="line line-bottom"></span>
+               </div>
+           </li>
+           <li class="nav-item">
+               <a href="#" class="nav-link nav-link-apple"></a>
+           </li>
+           <li class="nav-item">
+               <a href="#" class="nav-link nav-link-bag"></a>
+           </li>
+       </ul>
+
+       <!-- ========mobile ======== -->
+       <ul class="nav-list nav-list-larger">
+           
+           <li class="nav-item nav-item-hidden">
+               <a href="#" class="nav-link nav-link-apple"></a>
+           </li>
+           <li class="nav-item">
+               <a href="#" class="nav-link nav-link-bag"></a>
+           </li>
+       </ul>
+   </div>
+</div>
+  
+</template>
+
+<script>
+export default {
+    name: 'HeaderPage'
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+.container {
+    background-color: aqua;
+    width: 100%;
+    max-width: 1420px;
+    
+
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+ 
+    <HeaderPage />
+
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+}
+</script>
+<style lang="scss" scoped>
+* {
+  padding: 0;
+
+}
+</style>
